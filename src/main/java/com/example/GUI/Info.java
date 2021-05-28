d1c145e200661cf81aab13ba5f1760a311d62dd5
Hellboy-Aaryan <aaryan14032006@gmail.com>
2021-05-28 14:51:49 +0530

    Created Info Screen & Added File chooser

---
+++ src/main/java/com/example/GUI/Info.java
@@ -0,0 +1,97 @@
+package com.example.GUI;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+public class Info implements ActionListener {
+    JFrame frame = new JFrame();
+
+    JLabel mainpanel = new JLabel();
+
+    JTextField quesArea = new JTextField();
+
+    JButton submit = new JButton();
+    JButton choose = new JButton();
+
+    JFileChooser j = new JFileChooser();
+
+    // ------------------- variables ------------------------
+
+    private final int fwidth = 900;
+    private final int fheight = 700;
+
+    private final Color primaryColor = new Color(23, 23, 23);
+
+    Image image = new ImageIcon("src/main/java/com/example/res/background.jpg").getImage().getScaledInstance(fwidth,
+            fheight, Image.SCALE_SMOOTH);
+    ImageIcon icon = new ImageIcon(image);
+
+    public Info() {
+        quesArea.setBounds(300, 150, 400, 50);
+        quesArea.setOpaque(false);
+        quesArea.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.white));
+        quesArea.setForeground(Color.white);
+        quesArea.setFont(new Font("Aerial", Font.PLAIN, 20));
+        quesArea.setVisible(true);
+        quesArea.addActionListener(this);
+
+        submit.setBounds(400, 500, 150, 50);
+        submit.setText("Next  >");
+        submit.setFont(new Font("Comic Sans", Font.BOLD + Font.ITALIC, 30));
+        submit.setFocusable(false);
+        submit.setOpaque(false);
+        submit.setContentAreaFilled(false);
+        submit.setBorderPainted(false);
+        submit.setForeground(new Color(0, 255, 247));
+        submit.addActionListener(this);
+
+        choose.setBounds(200, 150, 100, 50);
+        choose.setText("Choose");
+        choose.setFont(new Font("Comic Sans", Font.BOLD, 20));
+        choose.setFocusable(false);
+        choose.setBorderPainted(false);
+        choose.setContentAreaFilled(false);
+        choose.setBorderPainted(false);
+        choose.setForeground(Color.white);
+        choose.addActionListener(this);
+
+        mainpanel.setBounds(0, 0, fwidth, fheight);
+        mainpanel.setIcon(icon);
+        mainpanel.setBackground(primaryColor);
+        mainpanel.setLayout(null);
+        mainpanel.add(quesArea);
+        mainpanel.add(submit);
+        mainpanel.add(j);
+
+        frame.setSize(fwidth, fheight);
+        frame.setTitle("Quiz Manager");
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(3);
+        frame.setResizable(false);
+        frame.add(mainpanel);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == choose) {
+            j.showSaveDialog(null);
+            j.setBounds(250, 150, 50, 50);
+            j.setOpaque(false);
+        }
+        if (e.getSource() == submit) {
+            frame.dispose();
+            new Frame();
+        }
+    }
+}
