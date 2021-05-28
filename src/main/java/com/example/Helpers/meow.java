352b39a9befb9bf60be9c14b24bd2c7c8c47ffa4
Hellboy-Aaryan <aaryan14032006@gmail.com>
2021-05-28 15:47:08 +0530

    Created new methods

--- src/main/java/com/example/Helpers/meow.java
+++ src/main/java/com/example/Helpers/meow.java
@@ -1,5 +1,11 @@
 package com.example.Helpers;
 
 public abstract interface meow {
+    public abstract void init();
+
     public abstract void start();
+
+    public abstract void addImageToList();
+
+    public abstract int rImage(int n);
 }
